bc75d40fbdce6786674e019e7b77570765e89f17
Rahime Tayfun <suheyladurna@gmail.com>
2019-11-13 15:26:56 -0600

    my js2 afternoon project

--- practice.js
+++ practice.js
@@ -13,7 +13,11 @@
   Give it a key of name with the value being your name, and another key of age with the value being your age.
   Then alert your name using dot notation.
 */
-
+let me={
+  name: 'Shayla',
+  age: 26,
+}
+alert(me.name)
 //Code here
 
 ////////// PROBLEM 2 //////////
@@ -22,21 +26,30 @@
   Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday.
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
-
+let favoriteThings={
+  band:'metallica',
+  food: 'pasta',
+  person: 'dad',
+  book: 'rich dad poor dad',
+  movie: 'jack ryan',
+  holiday: 'christmas'
+}
 //Code here
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
-
+favoriteThings.car = 'tesla';
+favoriteThings.brand = 'metallica';
 //Code here
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
-
+favoriteThings.food= 'Chicken Nuggets';
+favoriteThings.book= 'Harry Potter'
 //Code here
 
 ////////// PROBLEM 3 //////////
@@ -48,13 +61,19 @@
   Set the value of that key to 'chapstick'.
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
+let backPack={
 
+}
+
+let item = 'firstPocket';
+backPack[item]= 'chapstick';
+backPack.color= 'red';
 //Code here
 
 /*
   After you do the above, alert your entire backPack object.
 */
-
+alert(backPack)
 //Code here
 
 /*
@@ -62,7 +81,7 @@ You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
-
+console.log( backPack)
 //Code here
 
 ////////// PROBLEM 4 //////////
@@ -83,7 +102,8 @@ var user2 = {
   name -> 'Bryan G. Smith' and email -> 'bryan.smith@devmounta.in'.
   Make that change without modifying the original object code above.
 */
-
+user2.name='Bryan G. Smith';
+user2.email= 'bryan.smith@devmounta.in';
 //Code Here
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
@@ -94,20 +114,30 @@ var user2 = {
 /*
   Create an empty object called methodCollection.
 */
-
+let methodCollection={
+  
+}
 //Code Here
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
+methodCollection={
+  
+alertHello: function(){
+  alert('hello');},
+logHello: function(){
+  console.log('hello');}
+}
 
 //Code Here
 
 /*
   Now call your alertHello and logHello methods.
 */
-
+methodCollection.alertHello();
+methodCollection.logHello();
 //Code Here
 
 ////////// PROBLEM 6 //////////
@@ -116,7 +146,14 @@ var user2 = {
   Create a function called makePerson which takes in name, birthday, ssn as its parameters.
   Return a new object with all of the information that you passed in.
 */
-
+function makePerson(name,birthday,ssn){
+  let person={
+    name,
+    birthday,
+    ssn,
+  }
+  return person
+}
 //Code Here
 
 ////////// PROBLEM 7 //////////
@@ -125,5 +162,12 @@ var user2 = {
   Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
   Return that object so that whenever you invoke makeCard, you get a brand new credit card.
 */
-
+function makeCard(cardNumber,expirationDate, securityCode){
+  let creditCard={
+    cardNumber,
+    expirationDate,
+    securityCode,
+  }
+  return creditCard;
+}
 //Code Here
